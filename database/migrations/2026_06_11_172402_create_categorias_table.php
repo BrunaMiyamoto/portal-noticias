3c0a29effeadda53b5ea0c92a689c986613a0ade
Bruna Miyamoto <brumiyamoto00@gmail.com>
2026-06-11 14:42:40 -0300

    criação da tabela categoria

---
+++ database/migrations/2026_06_11_172402_create_categorias_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use phpDocumentor\Reflection\Types\Nullable;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('categorias', function (Blueprint $table) {
+            $table->id();
+            $table->string("nome", 80);
+            $table->string("descricao")->Nullable;
+            $table->string("cor", 7)->default("#EDEDED");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('categorias');
+    }
+};
